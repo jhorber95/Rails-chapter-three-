3784b6ee7d9da40fdda14618534fc7bd1341d375
Jhorkman Bernal <jhorber95@gmail.com>
2019-01-28 18:16:51 -0500

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -6,6 +6,10 @@ ruby '2.5.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
 gem 'bcrypt'
+gem 'faker'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
+
 gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[index edit update destroy]
+  before_action :correct_user, only: %i[edit update]
+  before_action :admin_user,     only: :destroy
+
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 12)
+  end
+
   def new
     @user = User.new
   end
@@ -11,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      remember user
+      remember @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -19,11 +28,48 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'Profile updated'
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user?(@user)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = 'Please log in'
+      redirect_to login_url
+    end
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
-
   end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
 end
